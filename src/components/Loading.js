f46a4f169d53ac5916f6f380219ca00c8e4a942f
Dag Gebreselasse <dgebreselasse@gmail.com>
2021-02-03 20:55:07 -0600

    made loading component background transparent. changed loading text and icon color to dark green.

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -4,11 +4,11 @@ import { useStateValue } from './StateWrap';
 
 function Loading() {
     const [{ loadingDisplay }] = useStateValue();
-    
+
     return (
         <div className="loading" style={{display: loadingDisplay}}>
             <div className="loading_row row">
-                <span className="fa fa-spinner fa-pulse fa-3x fa-fw text-primary col-12"></span>
+                <span className="fa fa-spinner fa-pulse fa-3x fa-fw col-12"></span>
                 <div className="col-12 text">Loading . . . </div>
             </div>
         </div>
